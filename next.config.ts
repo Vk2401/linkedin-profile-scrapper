6196ab84c393f4ab24c4ac00b3954bb05b571b6b
vasanth <vasanth@webronic.com>
2025-01-09 15:18:54 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
+const nextConfig: import('next').NextConfig = {
+
   async headers() {
     return [
       {
         source: "/api/:path*",
         headers: [
-          // Suppressing ESLint for wildcard origin usage - adjust this for production
-          /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          /* eslint-disable-next-line no-restricted-globals */
           { key: "Access-Control-Allow-Origin", value: "*" }, // Example: Adjust this as needed
           { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE" },
           { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type" },
